e5febe1c17fd46799bfbfa62d10ce6c35638b799
Majid Ali <majidomore@gmail.com>
2022-03-19 23:54:09 +0500

    head commented

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,14 +16,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <head>
+        {/* <head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap"
             rel="stylesheet"
           />
-        </head>
+        </head> */}
         <body className="debug-screens">
           <Main />
           <NextScript />
