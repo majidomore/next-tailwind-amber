b6f823931c1d5f8a9ad58125c1ba9b687af73772
Majid Ali <majidomore@gmail.com>
2022-03-21 20:04:36 +0500

    initial commit

---
+++ components/TeamAccounts.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  name?: string;
+};
+
+const TeamAccounts = ({ name }: Props) => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default TeamAccounts;
